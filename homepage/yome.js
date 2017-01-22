9a79edeadf813ba26dc77c143bdb3385064b2380
Makinori <maau3p@gmail.com>
2017-01-23 00:44:26 +0900

    [add] homepage
    mysterious homepage sample

---
+++ homepage/yome.js
@@ -0,0 +1,21 @@
+
+var text_var;
+var image_add;
+
+function talk_yome (moveing_id) {
+  
+  return 1;
+}
+
+
+
+function loop_yome() {
+  var moveing_id=0;
+  
+  
+ // setTimeout("loop_yome()", 500);
+}
+
+loop_yome();
+
+

92a769080c81b8f919aa9b296d2ac314320e83c6
i-makinori <maau3p@gmail.com>
2017-09-24 23:15:02 +0900

    [Add] functons(?)

---
+++ mcclim/file-browser.lisp
@@ -0,0 +1,79 @@
+(ql:quickload :mcclim)
+(ql:quickload :cl-fad)
+
+(in-package :clim-user)
+
+;;;; file-browser
+;; ref : McClim PDF guide-tour
+;; https://common-lisp.net/project/mcclim/static/documents/guided-tour.pdf
+;; file-browser
+
+;;;; frame/pane
+(define-application-frame file-browser ()
+  ((active-files :initform nil :accessor active-files))
+  (:panes
+   (file-browser :application
+                 :display-function '(dirlist-display-files)
+                 :display-time :command-loop)
+   (interactor :interactor))
+  (:layouts (default (vertically ()
+                       file-browser
+                       interactor)))
+  (:menu-bar t))
+
+(define-presentation-type dir-pathname ()
+  :inherit-from 'pathname)
+
+;; execute
+(defun run-file-browser ()
+  (run-frame-top-level (make-application-frame 'file-browser)))
+
+
+;;;; command
+
+(define-file-browser-command (com-edit-directory :menu t :name t)
+    ((dir 'dir-pathname))
+  (print dir)
+  (setf (active-files *application-frame*)
+        (cl-fad:list-directory dir)))
+
+(define-file-browser-command (com-parent-directory :menu t :name "Up")
+    ((dir 'dir-pathname))
+  (print dir)
+  (setf (active-files *application-frame*)
+        (cl-fad:list-directory (cl-fad:pathname-parent-directory dir))))
+
+(define-file-browser-command (com-quit :menu t :name t) ()
+  (frame-exit *application-frame*))
+
+;;;; UI of gadgets
+
+(defmethod dirlist-display-files ((frame file-browser) pane)
+  (clear-output-record (stream-output-history pane))
+  
+  (dolist (file (active-files frame))
+    (present file
+             (if (cl-fad:directory-pathname-p file) 'dir-pathname 'pathname)
+             :stream pane)
+    (terpri pane)))
+
+
+
+(define-presentation-to-command-translator pathname-to-edit-command
+    (dir-pathname
+     com-edit-directory
+     file-browser
+     :gesture :select
+     :documentation "edit this path")
+    (object)
+  (list object))
+
+
+(defmethod adopt-frame :after (frame-manager (frame file-browser))
+  (declare (ignore frame-manager))
+  (execute-frame-command frame
+                         `(com-edit-directory ,(make-pathname :directory '(:absolute)))))
+     
+
+
+

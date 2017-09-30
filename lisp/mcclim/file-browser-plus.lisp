7b54dcab1d0338af9d549df2845c694b078f9391
i-makinori <maau3p@gmail.com>
2017-09-30 17:13:34 +0900

    [Clean] directory/file

--- lisp/mcclim/file-browser-plus.lisp
+++ lisp/mcclim/file-browser-plus.lisp
@@ -1,22 +1,39 @@
+;; expand of sample file-browser of mcclim tutorial
+;;
+;; file-browser ;;
+;; ref : McClim PDF guide-tour
+;; https://common-lisp.net/project/mcclim/static/documents/guided-tour.pdf
+;; file-browser
+
 (ql:quickload :mcclim)
 (ql:quickload :cl-fad)
 
 (in-package :clim-user)
 
-;;;; file-browser
-;; ref : McClim PDF guide-tour
-;; https://common-lisp.net/project/mcclim/static/documents/guided-tour.pdf
-;; file-browser
+
+;; execute
+(defun run-file-browser-plus ()
+  (run-frame-top-level (make-application-frame 'file-browser)))
+
+;;;; util
+
+
 
 ;;;; frame/pane
+
 (define-application-frame file-browser ()
-  ((active-files :initform nil :accessor active-files))
+  ((active-files :initform nil :accessor active-files)
+   (current-dir :initform nil :accessor current-dir))
   (:panes
+   (path-manager (make-pane 'text-field
+                            :value ""
+                            :editable-p nil))
    (file-browser :application
                  :display-function '(dirlist-display-files)
                  :display-time :command-loop)
    (interactor :interactor))
   (:layouts (default (vertically ()
+                       path-manager
                        file-browser
                        interactor)))
   (:menu-bar t))
@@ -24,38 +41,34 @@
 (define-presentation-type dir-pathname ()
   :inherit-from 'pathname)
 
-;; execute
-(defun run-file-browser ()
-  (run-frame-top-level (make-application-frame 'file-browser)))
 
 
 ;;;; command
 
 (define-file-browser-command (com-edit-directory :menu t :name t)
     ((dir 'dir-pathname))
-  (print dir)
+  (setf (current-dir *application-frame*)
+        dir)
   (setf (active-files *application-frame*)
         (cl-fad:list-directory dir)))
 
-(define-file-browser-command (com-parent-directory :menu t :name "Up")
-    ((dir 'dir-pathname))
-  (print dir)
-  (setf (active-files *application-frame*)
-        (cl-fad:list-directory (cl-fad:pathname-parent-directory dir))))
-
 (define-file-browser-command (com-quit :menu t :name t) ()
   (frame-exit *application-frame*))
+
+
 
 ;;;; UI of gadgets
 
 (defmethod dirlist-display-files ((frame file-browser) pane)
   (clear-output-record (stream-output-history pane))
-  
+  (with-slots (path-manager current-dir) *application-frame*
+    (format t "::: ~A" current-dir))
   (dolist (file (active-files frame))
     (present file
              (if (cl-fad:directory-pathname-p file) 'dir-pathname 'pathname)
              :stream pane)
-    (terpri pane)))
+    (terpri pane)
+    ))
 
 
 

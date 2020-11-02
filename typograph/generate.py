c1b14e0d07621b02714dc8ccd7b5327540815188
Makinori Ikegami <maau3p@gmail.com>
2020-11-02 16:06:43 +0900

    old form kanji typograph

---
+++ typograph/generate.py
@@ -0,0 +1,47 @@
+
+from functools import reduce
+import math
+
+kanji_file_path = "./kanji-list.txt"
+
+kanji_f = open(kanji_file_path, mode="r")
+kanji_text = kanji_f.read()
+kanji_f.close()
+
+
+def list_to_2_tuple (lis, current=[]):
+    if len(lis)<2 :
+        return current
+    else :
+        return list_to_2_tuple(lis[2:],
+                               current+[tuple(lis[:2])])
+                               
+
+def split_kanji(text):
+    flatt_list = list(map(lambda c: c.replace(" ", ""),
+                          reduce(lambda a,b: a+b,
+                                 map(lambda l: l.split("|"), text.split("\n")))))
+    tuple_list = list_to_2_tuple(flatt_list)
+    return tuple_list
+
+
+num_col = 6
+
+def generate_list(kanji_tuple_list):
+    text = "| "
+    for row in range(math.ceil(len(kanji_tuple_list)/num_col)):
+        for col in range(num_col):
+            try:
+                tuple= kanji_tuple_list[row*num_col+col]
+                c = tuple[0] + " | " + tuple[1]
+            except:
+                c = "   |   "
+            text += c + " | "
+        text += "\n| "
+    return text[:-2]
+    
+
+splits = split_kanji(kanji_text)
+print(generate_list(splits))
+
+
